9ba909cfdb024dbc30a4c2fac0c84b6e0d85f526
Adrian Sinclair <adrian@adrusi.com>
2016-09-06 08:48:10 -0400

    implement custom argument parser

---
+++ src/Cli.js
@@ -0,0 +1,220 @@
+import fs from "fs";
+
+class Parser {
+    constructor() {
+        this._opts = [];
+        this._longs = Object.create(null);
+        this._shorts = Object.create(null);
+    }
+
+    register(long, short, hasArg) {
+        let record = { long, short, hasArg };
+        this._opts.push(record);
+        this._longs[long] = record;
+        if (short) this._shorts[short] = record;
+    }
+
+    parse(args) {
+        if (args == null) args = Array.from(process.argv.slice(2));
+        return _parse(args, [], []);
+    }
+
+    _parse(args, opts, tgts) {
+        if (args.length === 0) return { options: opts, targets: tgts };
+
+        let arg = args.shift();
+
+        if (arg === "--") {
+            while (args.length > 0) tgts.push(args.shift());
+            return _parse(args, opts, tgts);
+        }
+
+        if (arg.startsWith("--")) return _parseLong(arg, args, opts, tgts);
+        if (arg !== "-" && arg.startsWith("-")) return _parseShort(arg, args, opts, tgts);
+        
+        tgts.push(arg);
+        return _parse(args, opts, tgts);
+    }
+
+    _parseLong(arg, args, opts, tgts) {
+        let name, value;
+        arg.replace(/^--([^=]*)(?:=([\s\S]*))?$/, ($0, $1, $2) => {
+            name = $1;
+            value = $2;
+        });
+        if (name == null) throw new Error("failed parsing long argument");
+        
+        if (!this._longs.hasOwnProperty(name)) {
+            return {
+                error: "INVALID_OPTION",
+                option: name,
+                message: `invalid option supplied: '${arg}'`
+            };
+        }
+
+        let { hasArg } = this._longs[name];
+
+        if (!hasArg && value != null) {
+            return {
+                error: "UNNECESSARY_ARGUMENT",
+                option: name,
+                message: `unnecessary argument supplied: '${arg}'`
+            };
+        }
+        
+        if (!hasArg) {
+            opts.push({ name });
+            return _parse(args, opts, tgts);
+        }
+
+        if (hasArg && value != null) {
+            opts.push({ name, value });
+            return _parse(args, opts, tgts);
+        }
+
+        // hasArg && value == null
+        if (args.length === 0) {
+            return {
+                error: "MISSING_ARGUMENT",
+                option: name,
+                message: `no argument supplied: '${arg}'`
+            };
+        }
+
+        opts.push({ name, value: args.shift() });
+        return _parse(args, opts, tgts);
+    }
+
+    _parseShort(arg, args, opts, tgts) {
+        let chars = Array.from(arg.slice(1));
+
+        for (let opt = chars.shift(); chars.length > 0; opt = chars.shift()) {
+            if (!this._shorts.hasOwnProperty(opt)) {
+                return {
+                    error: "INVALID_OPTION",
+                    option: opt,
+                    message: `invalid option supplied: '${arg}' ('${opt}')`
+                };
+            }
+
+            let { long: name, hasArg } = this._shorts[opt];
+            if (!hasArg) opts.push({ name });
+            else {
+                if (chars.length === 0) {
+                    if (args.length === 0) {
+                        return {
+                            error: "MISSING_ARGUMENT",
+                            option: name,
+                            message: `no argument supplied: '${arg}'`
+                        };
+                    }
+
+                    opts.push({ name, value: args.shift() });
+                } else {
+                    opts.push({ name, value: chars.join("") });
+                }
+                break;
+            }
+        }
+
+        return _parse(args, opts, tgts);
+    }
+}
+
+const parser = new Parser();
+parser.register("register", null, false);
+parser.register("process", "p", false);
+parser.register("steps", null, true);
+parser.register("template", "t", true);
+parser.register("field", "f", true);
+parser.register("watch", "w", false);
+parser.register("recursive", "r", false);
+parser.register("output", "o", true);
+parser.register("list", "l", false);
+parser.register("after", "a", true);
+parser.register("before", "b", true);
+parser.register("keywords", null, true);
+parser.register("fields", null, true);
+parser.register("info", "i", false);
+parser.register("cancel", null, false);
+parser.register("replay", null, false);
+parser.register("reparse-template", null, false);
+parser.register("create-template", null, false);
+parser.register("template-info", null, false);
+parser.register("delete-template", null, false);
+parser.register("list-templates", null, false);
+parser.register("sort", null, true);
+parser.register("order", null, true);
+parser.register("bill", null, false);
+parser.register("key", "k", true);
+parser.register("secret", "s", true);
+parser.register("verbosity", "v", true);
+parser.register("verbose", null, false);
+parser.register("quiet", "q", false);
+parser.register("version", null, false);
+parser.register("help", "h", false);
+
+export default function cli(...args) {
+    let { options, targets } = parser.parse(...args);
+
+    let err = generalValidation(options);
+    if (err != null) return err;
+
+    return modeDispatch(options, targets);
+}
+
+const modes = [ "register", "process", "list", "info", "cancel", "replay",
+                "create-template", "template-info", "edit-template",
+                "delete-template", "list-templates", "bill", "help", "version" ];
+
+function generalValidation(options) {
+    let modesSpecified = [];
+    for (let option of options) {
+       if (modes.includes(option.name)) {
+           modesSpecified.push(option.name);
+           if (modesSpecified.length > 1) {
+               return {
+                   error: "MULTIPLE_MODES",
+                   option: option.name,
+                   message: `Mutually exclusive options specified: '${modesSpecified[0]}' and '${modesSpecified[1]}'`
+               };
+           }
+       }
+       
+       if (option.name === "field" && !option.value.match(/^[^=]+=[\s\S]*$/)) {
+           return {
+               error: "INVALID_OPTION",
+               option: option.name,
+               message: `invalid argument for --field: '${option.value}'`
+           };
+       }
+
+       if (option.name === "after" || option.name === "before") {
+           // TODO reject invalid dates
+       }
+
+       if (option.name === "sort" && !["id", "created", "modified"].includes(option.value)) {
+           return {
+               error: "INVALID_OPTION",
+               option: option.name,
+               message: `invalid argument for --sort`
+           };
+       }
+
+       if (option.name === "order" && !["asc", "desc"].includes(option.value)) {
+           return {
+               error: "INVALID_OPTION",
+               option: option.name,
+               message: `invalid argument for --order`
+           };
+       }
+
+       if (option.name === "verbosity" && !["0", "1", "2"].includes(option.value)) {
+           return {
+               error: "INVALID_OPTION",
+               option: option.name,
+               message: `invalid argument for --verbosity`
+           };
+       }
+    }
+}
