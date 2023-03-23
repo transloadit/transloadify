3916226c62fcf3ed1f62896963cd6050a8e863ed
Kevin van Zonneveld <kevin@vanzonneveld.net>
2023-03-23 20:11:39 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,15 @@ module.exports = {
   "env": {
     "node": true,
     "mocha": true
+  },
+  "rules": {
+    'no-console': ['off'],
+    // Temporarily disabled as we mass upgrade codebases to prettier and newer eslint versions:
+    'array-callback-return': ['off'],
+    'camelcase': ['off'],
+    'n/no-deprecated-api': ['off'],
+    'no-prototype-builtins': ['off'],
+    'no-unused-expressions': ['off'],
+    'prefer-const': ['off'],
   }
 }
